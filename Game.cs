db801b0d0053c5c92ae8ee3da02b3e8480fc551e
delokoseni <107359196+delokoseni@users.noreply.github.com>
2024-10-27 23:12:19 +0700

    Добавьте файлы проекта.

---
+++ Game.cs
@@ -0,0 +1,38 @@
+﻿using OpenTK;
+using OpenTK.Windowing.Desktop;
+using OpenTK.Graphics.OpenGL;
+using System;
+
+public class Game : GameWindow
+{
+    public Game() : base(new GameWindowSettings(), new NativeWindowSettings()
+    {
+        Size = new OpenTK.Mathematics.Vector2i(800, 600),
+        Title = "My OpenTK Game"
+    })
+    {
+    }
+
+    /**
+     * Вызывается при загрузке окна.
+     * Метод используется для инициализации настроек OpenGL.
+     */
+    protected override void OnLoad()
+    {
+        base.OnLoad();
+        GL.ClearColor(0.5f, 0.5f, 0.5f, 1.0f); // Цвет фона с альфа-каналом
+        GL.Enable(EnableCap.DepthTest); // Включаем тест глубины
+    }
+
+    /**
+     * Вызывается при каждом рендеринге кадра.
+     * Метод отвечает за отрисовку сцены. 
+     */
+    protected override void OnRenderFrame(OpenTK.Windowing.Common.FrameEventArgs e)
+    {
+        GL.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit);
+        // Рендеринг объектов здесь
+
+        SwapBuffers();
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+public class Program
+{
+    [STAThread]
+    public static void Main()
+    {
+        using (Game game = new Game())
+        {
+            game.Run(); 
+        }
+    }
+}
